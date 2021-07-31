6e997d9987acb9a1342648f1411535e60116bbe5
rinky101 <nayakrajalaxmi88@gmail.com>
2021-08-01 00:54:43 +0530

    fixed a bug in Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -20,6 +20,16 @@ def fact(num):
     return num
 print(fact(4))
 
+
+def sum(a,b):
+    return a+b
+
+n1= input("enter first name")
+n1=int(n1)
+n2=input("enter first name: ")
+n2=int(n2)
+print("sum is:",sum(n1,n2))
+
 #Approach3
 
 def fact(num):
